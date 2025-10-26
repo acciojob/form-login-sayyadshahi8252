1e1694773d40c7cc9a90bffa2f73de55f611418f
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-26 16:57:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault()
+	let firstnameval=document.getElementById("firstname").value;
+	let lastnameval=document.getElementById("lastname").value;
+	let cleanfirstname=firstnameval.trim()
+	let cleanlastname=lastnameval.trim()
+	alert(`${cleanfirstname} ${cleanlastname}`)
 
 }
